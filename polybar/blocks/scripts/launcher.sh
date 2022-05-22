b165c9b6964980bf47b587cf6171d9da009f62a1
artemis <hydroxyprop@gmail.com>
2022-05-23 00:37:30 +0200

    initial commit

---
+++ polybar/blocks/scripts/launcher.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+FILE="$HOME/.config/rofi/config.rasi"
+
+rofi -combi-modi window,drun,ssh -show combi
---
+++ vim/.vimrc
@@ -0,0 +1,5 @@
+set tabstop=4
+set relativenumber
+set number
+set ai
+set si
